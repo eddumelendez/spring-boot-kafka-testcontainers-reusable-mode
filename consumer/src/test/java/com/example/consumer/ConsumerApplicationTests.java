3b6845ea12155bf9bbe571dee7ea2362fe4054de
Eddú Meléndez <eddu.melendez@gmail.com>
2023-05-11 19:02:20 -0600

    Add spring-boot-devtools

--- consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.consumer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
-class ConsumerApplicationTests {
+public class ConsumerApplicationTests {
 
     public static void main(String[] args) {
         SpringApplication.from(ConsumerApplication::main)
@@ -20,6 +21,7 @@ class ConsumerApplicationTests {
 
         @Bean
         @ServiceConnection
+        @RestartScope
         KafkaContainer kafkaContainer() {
             return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.3.0"))
                     .withReuse(true);
--- producer/src/test/java/com/example/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
-class ProducerApplicationTests {
+public class ProducerApplicationTests {
 
 	public static void main(String[] args) {
 		SpringApplication.from(ProducerApplication::main)
@@ -20,6 +21,7 @@ class ProducerApplicationTests {
 
 		@Bean
 		@ServiceConnection
+		@RestartScope
 		KafkaContainer kafkaContainer() {
 			return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.3.0"))
 					.withReuse(true);
